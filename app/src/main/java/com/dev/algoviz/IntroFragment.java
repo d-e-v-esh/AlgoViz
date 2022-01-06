9238812b92825424fa60e55fe4966a190424fe27
Devesh <deves125@gmail.com>
2022-01-06 20:43:33 +0530

    New Font Added

--- app/src/main/java/com/dev/algoviz/IntroFragment.java
+++ app/src/main/java/com/dev/algoviz/IntroFragment.java
@@ -59,6 +59,11 @@ public class IntroFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
+
+
+
         return inflater.inflate(R.layout.fragment_intro, container, false);
     }
 }
--- app/src/main/java/com/dev/algoviz/MainActivity.java
+++ app/src/main/java/com/dev/algoviz/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().hide();
 
         viewPager2 = findViewById(R.id.viewPager2);
 
