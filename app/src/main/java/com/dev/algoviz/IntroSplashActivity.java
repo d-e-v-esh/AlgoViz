cf7863dbc74a6fea491680bd530c1932c82588ab
Devesh <deves125@gmail.com>
2022-01-08 01:48:26 +0530

    Dark Mode Prevented

--- app/src/main/java/com/dev/algoviz/IntroSplashActivity.java
+++ app/src/main/java/com/dev/algoviz/IntroSplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class IntroSplashActivity extends AppCompatActivity {
 
@@ -13,7 +14,8 @@ public class IntroSplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_intro_splash);
 
         getSupportActionBar().hide();
-
+        // Prevents dark mode being applied to this app
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         Thread thread = new Thread() {
 
